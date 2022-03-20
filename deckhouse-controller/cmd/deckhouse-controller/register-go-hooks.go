7317b2599ef219d1d681618c28e65ecc91f8d82f
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-03-20 21:32:54 +0300

    ++

--- deckhouse-controller/cmd/deckhouse-controller/register-go-hooks.go
+++ deckhouse-controller/cmd/deckhouse-controller/register-go-hooks.go
@@ -49,6 +49,7 @@ import (
 	_ "github.com/deckhouse/deckhouse/modules/340-monitoring-custom/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/340-monitoring-kubernetes/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/340-monitoring-ping/hooks"
+	_ "github.com/deckhouse/deckhouse/modules/340-monitoring-yandex-cloud/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/400-descheduler/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/402-ingress-nginx/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/460-log-shipper/hooks"
